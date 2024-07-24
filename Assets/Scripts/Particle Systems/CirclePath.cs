744ebd86064258f5b229fc8ff3113e8f7f3471f9
Kauê Reis <68311889+kauer3@users.noreply.github.com>
2024-07-24 15:36:51 +1200

    First commit

---
+++ Assets/Scripts/Particle Systems/CirclePath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CirclePath : MonoBehaviour {
+
+	public Transform centerPoint;
+	public float speed;
+	public float radius;
+	public bool x,y,z;
+
+	void Start(){
+		transform.position = new Vector3 (transform.position.x - radius, transform.position.y, transform.position.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (x) {
+			transform.RotateAround (centerPoint.position, transform.right, speed * Time.deltaTime);
+		}
+		if (y) {
+			transform.RotateAround (centerPoint.position, transform.up, speed * Time.deltaTime);
+		}
+		if (z) {
+			transform.RotateAround (centerPoint.position, transform.forward, speed * Time.deltaTime);
+		}
+	}
+}
+
+[CustomEditor(typeof(CirclePath))]
+public class DrawCirclePath : Editor {
+	void OnSceneGUI(){
+		Handles.color = Color.green;
+		CirclePath myObj = (CirclePath)target;
+		Handles.DrawWireDisc (myObj.centerPoint.position, myObj.transform.up, myObj.radius);
+	}
+}
